15b14a5e1e6922a32e6e683be8603886c849087e
Braulio Moroni <moroniraziel@gmail.com>
2024-09-26 15:44:46 -0300

    Data load done and DataFrame presented

---
+++ app.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+def load_data():
+    """
+    Load the data from the processed bikes dataset.
+    Returns
+    -------
+    pandas.DataFrame
+        The loaded data.
+    """
+    return pd.read_csv("data/processed/bikes_completed.csv")
+
+df = load_data()
+
+st.dataframe(df)
